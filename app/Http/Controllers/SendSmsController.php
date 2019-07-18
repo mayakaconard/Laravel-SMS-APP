0cc16e9fc0344c9acea2d63946f057b05ab6f5a1
mayakaconard <mayakaconard@gmail.com>
2019-07-18 08:58:17 +0300

    cleaned the repo

--- app/Http/Controllers/SendSmsController.php
+++ app/Http/Controllers/SendSmsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class SendSmsController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('app.sendsms');
+    }
 }
---
+++ resources/views/app/sendsms.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts/master')
+
+@section('content')
+<section>
+    <!-- START Page content-->
+    <section class="main-content">
+        <button type="button" class="btn btn-labeled btn-primary pull-right">
+            <span class="btn-label"><i class="fa fa-plus-circle"></i>
+            </span>Add Item</button>
+        <h3>
+            Dashboard
+            <br>
+            <small>Welcome user</small>
+        </h3>
+        <div class="row">
+            <!-- START Send sms content content-->
+
+
+        </div>
+    </section>
+    <!-- END Page content-->
+</section>
+<!-- END Main section-->
+</section>
+
+@endsection
